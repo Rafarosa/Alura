403eab6471a0ecb6f81caa1db53e3cb5d6e39206
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2022-04-05 22:02:17 -0300

    02 - função JS

--- Iniciante em Programação/JavaScript para Web-Crie paginas dinamicas/main.js
+++ Iniciante em Programação/JavaScript para Web-Crie paginas dinamicas/main.js
@@ -1,1 +1,9 @@
-document.querySelector('.tecla_pom');
+function tocasSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocasSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocasSomPom;
