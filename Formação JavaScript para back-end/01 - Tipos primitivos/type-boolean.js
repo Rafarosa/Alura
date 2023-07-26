050e6a4e4ec890898ee3533cd485f16071c7197f
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-07-25 22:05:33 -0300

    inicio do curso JS back-end

---
+++ Formação JavaScript para back-end/01 - Tipos primitivos/type-boolean.js
@@ -0,0 +1,12 @@
+// Boolean
+// true e false
+const primeiroNumero = 5;
+const seguntoNumero = 5;
+let cadastroAtivo = true;
+
+//console.log(primeiroNumero === seguntoNumero);
+
+const texto01 = "Alura";
+const texto02 = "alura";
+
+console.log(texto01 === texto02);
---
+++ Formação JavaScript para back-end/01 - Tipos primitivos/type-string.js
@@ -0,0 +1,17 @@
+const texto01 = "Olá mundo!";
+const texto02 = 'Olá mundo!';
+
+const senha = "senhaSuperSegura457@"
+
+const stringDenumeros = "3456789"
+
+const citacao = "Meu nome é ";
+const meuNome = "Rafael"
+console.log(citacao + meuNome)
+
+// Concatenação  (+)
+
+
+
+// Template String ou Template literal
+
