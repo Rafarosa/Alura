992b0a41d96f09d9f5213f40582415cd31aa1d34
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-08-06 16:36:27 -0300

    JavaScript -  inicio

---
+++ Formação JavaScript para back-end/02 - Declarando variáveis/02_variaveis.js
@@ -0,0 +1,37 @@
+//Variaveis
+
+//var
+// var altura = 5;
+// var comprimento = 7;
+
+// var area = altura * comprimento;
+// console.log(area);
+// pode ser decalrado com var apos o delimitação da instrução 
+
+//Let
+
+// let forma = 'retângulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if(forma === 'retângulo') {
+//   area = altura * comprimento;
+// }else{
+//   area = altura * comprimento / 2;
+// }
+// console.log(area)
+
+// const
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if(forma === 'quadrado') {
+  area = altura * comprimento;
+}else {
+  area = (altura * comprimento) / 2
+}
+console.log(area)
---
+++ Formação JavaScript para back-end/03 - JS e o NodeJS/console.js
@@ -0,0 +1,11 @@
+// log -> registro
+
+const minhaVariavel = true;
+
+console.log(234);
+console.log('Eu sou um texto')
+console.log(minhaVariavel)
+
+// tratamento de erro
+
+console.error('Deu erro...')
