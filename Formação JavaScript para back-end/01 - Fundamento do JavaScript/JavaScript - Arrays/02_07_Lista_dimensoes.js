514e494a0112bb8114fb074f07e755768460c34f
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-08-21 21:26:26 -0300

    Aula sobre Array

---
+++ Formação JavaScript para back-end/01 - Fundamento do JavaScript/JavaScript - Arrays/02_07_Lista_dimensoes.js
@@ -0,0 +1,18 @@
+// interirando dados com duas dimensão
+
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosMedisa = [alunos, medias];
+console.log(listaDeAlunosMedisa);
+
+console.log(`a Aluna na posção 1 da lista Alunos é: ${listaDeAlunosMedisa[0][1]}
+A nota dessa aluna é ${listaDeAlunosMedisa[1][1]}`);
+
+
+const nomes = ["Ana", "Juliana", "Leonardo"];
+const idades = [30, 35, 28];
+const faculdade = [false, true, true];
+
+const funcionarios = [nomes, idades, faculdade];
+
